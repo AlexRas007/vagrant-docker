4fe44f54b454187f9bf230bf88a6b4326762674a
Alexandr <28950726+AlexRas007@users.noreply.github.com>
2023-09-01 18:31:54 +0400

    Git init

--- commands/vm_command.rb
+++ commands/vm_command.rb
@@ -6,17 +6,9 @@ if ARGV[0] == "vm" then
 	command = ARGV[1]
 	subcommand = ARGV[2]
 
-	# vagrant vm clean-files - очищает файлы работы полностью
-	if command == "files" && command == "clean" then
-		system("git clean -dfX --exclude=\!\"Vagrantconfig.yaml\" --exclude=\!\".idea/*\" --exclude=\!\".vagrant\"")
-		exit
-	end
-
-	# vagrant vm reinstall - полностью переустанавливает виртуальную машину с очисткой файлов работы полностью
+	# vagrant vm reinstall - полностью переустанавливает виртуальную машину
 	if command == "reinstall" then
 		system("vagrant destroy -f")
-		puts "\n---------------------\n\n"
-		system("vagrant vm clean-files")
 		puts "\n---------------------\n\n"
 		system("vagrant up")
 		exit
@@ -24,7 +16,6 @@ if ARGV[0] == "vm" then
 
 	puts "Usage: vagrant vm <command>\n\n"
 	puts "Commands:"
-	puts "	reinstall      переустанавливает виртуальную машину с очисткой файлов работы полностью"
-	puts "	files clean    очищает файлы работы полностью"
+	puts "	reinstall      переустанавливает виртуальную машину"
 	exit
 end
